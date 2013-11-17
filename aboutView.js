a3a9272359519129bef605c7be9442438e7fd4aa
Josh <josh@rezoundmusic.com>
2013-11-17 11:51:19 -0500

    still trying to get the view to load

--- aboutView.js
+++ aboutView.js
@@ -1,7 +1,7 @@
 var AboutUsView = Backbone.View.extend({
 	el: $('#display'),
 	initialize: function(){
-		this.render;
+		router.on("route:about", this.render);
 	},
 	render: function(){
 		this.$el.empty().append("<p>About Us - placeholder</p>");
