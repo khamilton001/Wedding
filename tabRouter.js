4f82cb37c59e3c873a6b6b147d9cacfbd1fb9517
Josh <josh@rezoundmusic.com>
2013-11-17 11:39:16 -0500

    minor change

--- tabRouter.js
+++ tabRouter.js
@@ -4,30 +4,30 @@ var tabRouter = Backbone.Router.extend({
 		'directions/' : 'directions',
 		'accomodations/' : 'accomodations',
 		'registry/' : 'registry',
-		'about/' : 'aboutUs',
+		'about/:id' : 'aboutUs',
 		'proposal/' : 'proposal',
 		'pictures/' : 'pictures'
 	},
-importantInfo : function(){
-	var importantInfoView = new ImportantInfoView;
-},
-directions : function(){
-	var directionsView = new DirectionsView;
-},
-accomodations : function(){
-	var accomodationsView = new AccomodationsView;
-},
-registry : function(){
-	var registryView = new RegistryView;
-},
-aboutUs : function(){
-	var aboutUsView = new AboutUsView();
-},
-proposal : function(){
-	var proposalView = new ProposalView;
-},
-pictures : function(){
-	var picturesView = new PicturesView;
-}
+	importantInfo : function(){
+		var importantInfoView = new ImportantInfoView;
+	},
+	directions : function(){
+		var directionsView = new DirectionsView;
+	},
+	accomodations : function(){
+		var accomodationsView = new AccomodationsView;
+	},
+	registry : function(){
+		var registryView = new RegistryView;
+	},
+	aboutUs : function(id){
+		var aboutUsView = new AboutUsView();
+	},
+	proposal : function(){
+		var proposalView = new ProposalView;
+	},
+	pictures : function(){
+		var picturesView = new PicturesView;
+	}
 
 });
