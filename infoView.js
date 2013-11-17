5e45228cb00a6c6d0b257901df67e8ef8a866836
Kat <katherinehamilton68@gmail.com>
2013-11-17 10:53:18 -0500

    placeholder

---
+++ infoView.js
@@ -0,0 +1,9 @@
+var ImportantInfoView = Backbone.View.extend({
+	el: $('#display'),
+	initialize: function(){
+		this.render);
+	},
+	render: function(){
+		this.$el.empty().append("<p>Information about the venue, hotels, other important information</p>");
+	}
+});
