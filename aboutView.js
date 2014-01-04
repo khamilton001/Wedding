20da254c9097f09fe92dec6149acd6e0536b051f
khamilton001 <katherinehamilton68@gmail.com>
2014-01-04 16:55:18 -0500

    actual stuff

--- aboutView.js
+++ aboutView.js
@@ -4,6 +4,12 @@ var AboutUsView = Backbone.View.extend({
 		this.render();
 	},
 	render: function(){
-		this.$el.empty().append("<p>About Us - placeholder</p>");
+		this.$el.empty().append("<p>About Us<br>
+								<strong>Josh Wickman</strong><br>
+								Josh grew up in Endicott, NY.  After completing his bachelor's degree in physics, he moved to Newark, DE to pursue his Ph.D. in particle physics and cosmology.  He completed his degree in August 2013 and currently teaches physics courses at Gloucester County College in NJ.<br>
+								<strong>Kat Hamilton</strong><br>
+								Kat is originally from Sussex, NJ.  She completed her bachelor's in music at Drew University and moved to Philadelphia to get her master's in music education from University of the Arts.  She currently teaches 3 and 4 year olds at a private preschool in Center City.<br>
+								Josh and Kat met at a mutual friend's Christmas party in 2009.  They reconnected on a canoeing trip in Vermont in July 2010 and started dating shortly after.  Together, Josh and Kat enjoy playing music, cooking tasty meals and making their own beer.
+								</p>");
 	}
 });
