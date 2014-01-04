e34ea92688340482496aa8ff31e51a19f80f3ef7
khamilton001 <katherinehamilton68@gmail.com>
2014-01-04 17:30:25 -0500

    fix

--- infoView.js
+++ infoView.js
@@ -4,9 +4,6 @@ var ImportantInfoView = Backbone.View.extend({
 		this.render();
 	},
 	render: function(){
-		this.$el.empty().append('<p>Information about the venue, hotels, other important information<br>
-								<strong>Map to Venue - The Barn</strong><br>
-								<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;hl=en&amp;q=1409+Ithaca+Rd+13864&amp;ie=UTF8&amp;om=1&amp;hq=&amp;hnear=1409+Ithaca+Rd,+Willseyville,+New+York+13864&amp;t=m&amp;z=10&amp;ll=42.309815,-76.400986&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/maps?f=q&amp;hl=en&amp;q=1409+Ithaca+Rd+13864&amp;ie=UTF8&amp;om=1&amp;hq=&amp;hnear=1409+Ithaca+Rd,+Willseyville,+New+York+13864&amp;t=m&amp;z=10&amp;ll=42.309815,-76.400986&amp;source=embed" style="color:#0000FF;text-align:left">View Larger Map</a></small>
-								</p>');
+		this.$el.empty().append('<p>Information about the venue, hotels, other important information</p>');
 	}
 });
