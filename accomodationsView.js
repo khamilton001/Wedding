610a54b189d80f3c368c4ba9eeca083b47774b33
Kat <katherinehamilton68@gmail.com>
2014-01-19 12:25:54 -0500

    text added

--- accomodationsView.js
+++ accomodationsView.js
@@ -4,6 +4,6 @@ var AccomodationsView = Backbone.View.extend({
 		this.render();
 	},
 	render: function(){
-		this.$el.empty().append('<p></p>');
+		this.$el.empty().append('<p>We will be reserving hotel blocks in the area soon.  Check back for hotel information and directions!</p>');
 	}
 });
