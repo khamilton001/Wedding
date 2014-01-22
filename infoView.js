25d45f22e84172f0cefbf33175e522f56399f208
Kat <katherinehamilton68@gmail.com>
2014-01-22 10:08:59 -0500

    balls

--- infoView.js
+++ infoView.js
@@ -4,6 +4,6 @@ var ImportantInfoView = Backbone.View.extend({
 		this.render();
 	},
 	render: function(){
-		this.$el.empty().append('<p><h2>Information</h2><br><br>Information about the venue, hotels, other important information</p>');
+		this.$el.empty().append('<p><h2>Information</h2>Information about the venue, hotels, other important information</p>');
 	}
 });
