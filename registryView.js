fcb7f2b9df20974545fbc2990840b03e38ffecfe
Kat <katherinehamilton68@gmail.com>
2014-01-22 09:04:59 -0500

    heading

--- registryView.js
+++ registryView.js
@@ -4,6 +4,6 @@ var RegistryView = Backbone.View.extend({
 		this.render();
 	},
 	render: function(){
-		this.$el.empty().append('<p>We have an official registry with Kohls <a href="http://www.kohls.com/upgrade/gift_registry/kohlsgrw_home.jsp?section=list&listid=2650492&viewtype=listview&isfromsearch=true&fname=katherine&lname=hamilton&regtype=registry.wedding,registry.baby,registry.splday">here</a>.  You can also search "Katherine Hamilton" and find us.<br>We also have a Zola registry for miscellaneous items from other websites <a href="www.zola.com/registry/katherineandjoshua">here</a></p>');
+		this.$el.empty().append('<p><h2>Registry</h2>We have an official registry with Kohls <a href="http://www.kohls.com/upgrade/gift_registry/kohlsgrw_home.jsp?section=list&listid=2650492&viewtype=listview&isfromsearch=true&fname=katherine&lname=hamilton&regtype=registry.wedding,registry.baby,registry.splday">here</a>.  You can also search "Katherine Hamilton" and find us.<br><br>We also have a Zola registry for miscellaneous items from other websites <a href="www.zola.com/registry/katherineandjoshua">here</a>.</p>');
 	}
 });
