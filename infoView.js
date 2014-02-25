991b352412f4f79ba1cbd738f328221d5e4cf8da
Kat <katherinehamilton68@gmail.com>
2014-02-24 19:28:56 -0500

    better placeholder text

--- infoView.js
+++ infoView.js
@@ -4,6 +4,6 @@ var ImportantInfoView = Backbone.View.extend({
 		this.render();
 	},
 	render: function(){
-		this.$el.empty().append('<p><h2>Information</h2>Information about the venue, hotels, other important information</p>');
+		this.$el.empty().append('<p><h2>Information</h2>We\'re still working out some details about our ceremony and reception!  Please check back for more information.  We\'ll have it all soon, we promise!</p>');
 	}
 });
