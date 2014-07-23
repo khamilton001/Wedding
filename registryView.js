e7dd8ec327bc2e9e85bd91122b108c059409c761
Kat <katherinehamilton68@gmail.com>
2014-07-22 20:10:21 -0400

    test

--- registryView.js
+++ registryView.js
@@ -4,6 +4,6 @@ var RegistryView = Backbone.View.extend({
 		this.render();
 	},
 	render: function(){
-		this.$el.empty().append('<p><h2>Registry</h2><p>We have registries with Kohl\'s and Zola.  Click on the logos to visit our registries!</p><br><a href="http://www.kohls.com/upgrade/gift_registry/kohlsgrw_home.jsp?section=list&listid=2650492&viewtype=listview&isfromsearch=true&fname=katherine&lname=hamilton&regtype=registry.wedding,registry.baby,registry.splday"><img src="Kohlslogo.jpg" alt="Kohls" align="middle"></a><br><br><a href="http://zola.com/registry/katherineandjoshua"><img src="zolalogo.jpg" alt="Zola"></a>.<br><p>Please make sure that when you purchase an item it is removed or marked as purchased on the registry.  Thank you!</p></p>');
+		this.$el.empty().append('<p><h2>Registry</h2><p>We have registries with Kohl\'s and Zola.  Click on the logos to visit our registries!</p><br><a href="http://www.kohls.com/upgrade/gift_registry/kohlsgrw_home.jsp?section=list&listid=2650492&viewtype=listview&isfromsearch=true&fname=katherine&lname=hamilton&regtype=registry.wedding,registry.baby,registry.splday"><img src="Kohlslogo.jpg" alt="Kohls" align="middle"></a><br><br><a href="http://zola.com/registry/katherineandjoshua"><img src="zolalogo.jpg" alt="Zola"></a>.<br><br><p>Please make sure that when you purchase an item it is removed or marked as purchased on the registry.  Thank you!</p></p>');
 	}
 });
